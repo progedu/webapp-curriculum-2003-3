877644ac1c4053c753c18b1d06090f55c0f45606
nonoike <ij4nu8d4fw@gmail.com>
2017-11-09 22:18:30 +0900

    簡易的な Option を実装

--- src/main/scala-2.11/OptionStudy.scala
+++ src/main/scala-2.11/OptionStudy.scala
@@ -1,11 +1,20 @@
 object OptionStudy {
 
   trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(x) => Some(f(x))
+    }
 
-    def getOrElse[B >: A](default: => B): B = ???
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(x) => x
+    }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case None => None
+      case Some(x) => f(x)
+    }
   }
 
   case class Some[+A](get: A) extends Option[A]
