10e527935c383cfee8bdf318d89add56cc33eafb
apprec100 <apprec100@outlook.com>
2018-04-16 15:02:24 +0900

    簡易的な Option 型を実装した

--- src/main/scala-2.11/OptionStudy.scala
+++ src/main/scala-2.11/OptionStudy.scala
@@ -1,15 +1,23 @@
 object OptionStudy {
 
-  trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+  sealed trait Option[+A] {
+    final def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(a) => Some(f(a))
+      case _ => None
+    }
 
-    def getOrElse[B >: A](default: => B): B = ???
+    final def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(a) => a
+      case _ => default
+    }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    final def flatMap[B](f: A => Option[B]): Option[B] =
+      this.map(f).getOrElse(None)
   }
 
   case class Some[+A](get: A) extends Option[A]
 
   case object None extends Option[Nothing]
-
 }
