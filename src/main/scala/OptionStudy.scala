d4b869a71055720211d025611bb885f20ae9bf3b
Masahiro <masahiro.infinite77@gmail.com>
2018-12-08 03:13:15 +0900

    map, getOrElse, flatMapの実装

--- src/main/scala/OptionStudy.scala
+++ src/main/scala/OptionStudy.scala
@@ -1,11 +1,26 @@
 object OptionStudy {
 
   trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+    def map[B](f: A => B): Option[B] = {
+      this match {
+        case a: Some[A] => Some(f(a.get))
+        case None => None
+      }
+    }
 
-    def getOrElse[B >: A](default: => B): B = ???
+    def getOrElse[B >: A](default: => B): B = {
+      this match {
+        case a: Some[B] => a.get
+        case None => default
+      }
+    }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    def flatMap[B](f: A => Option[B]): Option[B] = {
+      this match {
+        case a: Some[A] => f(a.get)
+        case None => None
+      }
+    }
   }
 
   case class Some[+A](get: A) extends Option[A]
