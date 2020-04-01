385693a15741d8255af6527afc7380f8722fac2d
QWYNG <ikusawasi@gmail.com>
2020-04-01 21:52:12 +0900

    impl OptionStudy

--- src/main/scala/OptionStudy.scala
+++ src/main/scala/OptionStudy.scala
@@ -1,11 +1,29 @@
 object OptionStudy {
 
   trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+    def map[B](f: A => B): Option[B] =
+      if (this == None) {
+        None
+      } else {
+        val Some(e) = this
+        Some(f(e))
+      }
 
-    def getOrElse[B >: A](default: => B): B = ???
+    def getOrElse[B >: A](default: => B): B =
+      if (this == None) {
+        default
+      } else {
+        val Some(e) = this
+        e
+      }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      if (this == None) {
+        None
+      } else {
+        val Some(e) = this
+        f(e)
+      }
   }
 
   case class Some[+A](get: A) extends Option[A]
