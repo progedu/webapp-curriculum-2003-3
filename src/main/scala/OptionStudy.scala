266aba7993145bf6fc38782c6c8db026273d363f
Ryuji Yamamoto <45560446+ryamay@users.noreply.github.com>
2021-06-13 23:26:01 +0900

    Update OptionStudy.scala

--- src/main/scala/OptionStudy.scala
+++ src/main/scala/OptionStudy.scala
@@ -1,11 +1,20 @@
 object OptionStudy {
 
   trait Option[+A] {
-    def map[B](f: A => B): Option[B] = ???
+    def map[B](f: A => B): Option[B] = this match {
+      case Some(value) => Some(f(value))
+      case None => None
+    }
+    
+    def getOrElse[B >: A](default: => B): B = this match {
+      case Some(value) => value
+      case None => default
+    }
 
-    def getOrElse[B >: A](default: => B): B = ???
-
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case Some(value) => f(value)
+      case None => None
+    }
   }
 
   case class Some[+A](get: A) extends Option[A]
